1b510bf1415df50fe6d005be99ef4c47a21f364d
Maria Mair <mm225mz@student.lnu.se>
2025-10-13 23:48:56 +0200

    Add pre-selected values

--- public/js/index.js
+++ public/js/index.js
@@ -7,13 +7,28 @@
 
 const form = document.querySelector('#trainingInformation')
 
+setTodaysDate()
+setDefaultMinutes(60)
+
+function setTodaysDate() {
+  let today = new Date(Date.now())
+  today = today.toISOString()
+  today = today.substring(0, 10)
+
+  form.querySelector('#date').setAttribute('value', today)
+  form.querySelector('#date').setAttribute('max', today)
+}
+
+function setDefaultMinutes(minutes) {
+  form.querySelector('#minutes').setAttribute('value', minutes)
+}
+
 form.addEventListener('submit', (event) => {
   saveInformationAndReloadPage(event)
 })
 
 async function saveInformationAndReloadPage(event) {
   event.preventDefault()
-  console.log('har submittats')
   const values = readValues(event.target)
   const result = await saveInformation(values)
   console.log('result: ' + result)
@@ -22,6 +37,7 @@ async function saveInformationAndReloadPage(event) {
 
 function readValues (form) {
   const values = { 
+    username: form.querySelector('#username').value,
     date: form.querySelector('#date').value,
     type: form.querySelector('#type').value,
     minutes: form.querySelector('#minutes').value
