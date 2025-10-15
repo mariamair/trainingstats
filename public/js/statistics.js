09c7a286286f8edb33b7204de085efad8190d851
Maria Mair <mm225mz@student.lnu.se>
2025-10-15 19:06:43 +0200

    Add statistics display

---
+++ public/js/statistics.js
@@ -0,0 +1,94 @@
+/**
+ * The script file of the statistics page.
+ *
+ * @author Maria Mair <mm225mz@student.lnu.se>
+ */
+
+const form = document.querySelector('#statistics')
+const container = document.querySelector('#trainingStatistics')
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault()
+  const option = form.querySelector('select').value
+  const result = await getInformation(option)
+  displayStatistics(option, result)
+})
+
+async function getInformation (option) {
+  try {
+    const res = await window.fetch(`/statistics/${option}`)
+  
+    if (!res.ok) {
+      throw new Error ('Could not fetch information')
+    } 
+
+    const data = await res.json()
+
+    return JSON.parse(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+function displayStatistics(option, result) {
+  if (option === 'totalTime') {
+    displayTotalTime(result)
+  }
+  if (option === 'histogram') {
+    createHistogram(result)
+  }
+}
+
+function displayTotalTime(result) {
+  clearDisplay()
+  displayHeading('Your total training time: ' + result + ' minutes')
+}
+
+function clearDisplay() {
+  while (container.firstChild) {
+    container.firstChild.remove()
+  }
+}
+
+function displayHeading(text) {
+  const heading = document.createElement('h3')
+  heading.textContent = text
+  container.appendChild(heading)
+}
+
+function createHistogram (intervals) {
+  clearDisplay()
+  displayHeading('Minutes per training instance')
+
+  let histogramClone
+  const histogramTemplate = document.querySelector('#histogram')
+
+  for (const interval of intervals) {
+    histogramClone = histogramTemplate.content.cloneNode(true)
+    displayHistogram(histogramClone, interval)
+    displayBoundaries(histogramClone, interval)
+    displayDataPoints(histogramClone, interval)
+    container.appendChild(histogramClone)
+  }
+}
+
+function displayHistogram(clone, interval) {
+  const swatch = clone.querySelector('.interval-swatch')
+  swatch.style.backgroundColor = interval.color.hexValue
+  swatch.style.width = 50 * getNumberOfDataPoints(interval.data) + 'px'
+}
+
+function displayBoundaries (clone, interval) {
+  const boundaries = clone.querySelector('.interval-boundaries')
+  boundaries.textContent = interval.lowerBoundary + ' - ' + interval.upperBoundary
+}
+
+function displayDataPoints(clone, interval) {
+  const dataPoints = clone.querySelector('.interval-datapoints')
+  dataPoints.textContent = 'Number of instances: ' + interval.data.length
+}
+
+function getNumberOfDataPoints (dataPoints) {
+  return dataPoints.length
+}
+
