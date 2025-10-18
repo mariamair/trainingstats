0ad4bdd7b6b1151fb33a2cf709d9ca491dd8f494
Maria Mair <mm225mz@student.lnu.se>
2025-10-18 17:43:08 +0200

    Enable browser globals

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import js from '@eslint/js'
+import globals from 'globals'
 import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
@@ -23,6 +24,11 @@ export default defineConfig([
       'max-params': ['warn', { 'max': 3 }],
       'object-curly-spacing': ['error', 'always'],
       quotes: ['error', 'single']
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser
+      }
     }
   }
 ])
